b32d2c005dded4977858996c043d49dcc69b0df5
Brennon Jacobson <brennonj@me.com>
2025-11-19 00:12:12 -0800

    update theme and add harpoon

--- init.lua
+++ init.lua
@@ -882,20 +882,23 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
+    'maxmx03/solarized.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     config = function()
-      ---@diagnostic disable-next-line: missing-fields
-      require('tokyonight').setup {
+      require('solarized').setup {
+        transparent = { enabled = false },
         styles = {
-          comments = { italic = false }, -- Disable italics in comments
+          comments = { italic = false },
+          functions = { italic = false },
+          variables = { italic = false },
         },
       }
 
       -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      -- You can use 'solarized' for light mode or pass variant option
+      vim.cmd.colorscheme 'solarized'
+      -- To explicitly set dark mode:
+      vim.o.background = 'dark'
     end,
   },
 
--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -86,4 +86,48 @@ return {
       end, { desc = '[C]opilot [Q]uick Chat' })
     end,
   },
+  {
+    'ThePrimeagen/harpoon',
+    branch = 'harpoon2',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
+      local harpoon = require 'harpoon'
+      harpoon:setup()
+
+      -- Keymaps
+      vim.keymap.set('n', '<leader>a', function()
+        harpoon:list():add()
+      end, { desc = 'Harpoon [A]dd file' })
+
+      vim.keymap.set('n', '<C-e>', function()
+        harpoon.ui:toggle_quick_menu(harpoon:list())
+      end, { desc = 'Harpoon toggle menu' })
+
+      -- Jump to specific files
+      vim.keymap.set('n', '<C-h>', function()
+        harpoon:list():select(1)
+      end, { desc = 'Harpoon file 1' })
+
+      vim.keymap.set('n', '<C-t>', function()
+        harpoon:list():select(2)
+      end, { desc = 'Harpoon file 2' })
+
+      vim.keymap.set('n', '<C-n>', function()
+        harpoon:list():select(3)
+      end, { desc = 'Harpoon file 3' })
+
+      vim.keymap.set('n', '<C-s>', function()
+        harpoon:list():select(4)
+      end, { desc = 'Harpoon file 4' })
+
+      -- Toggle previous & next buffers stored within Harpoon list
+      vim.keymap.set('n', '<C-S-P>', function()
+        harpoon:list():prev()
+      end, { desc = 'Harpoon previous' })
+
+      vim.keymap.set('n', '<C-S-N>', function()
+        harpoon:list():next()
+      end, { desc = 'Harpoon next' })
+    end,
+  },
 }
